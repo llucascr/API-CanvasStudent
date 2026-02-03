e251e6d498c36031e2aa1d5eba22a9613d1ef770
llucascr <lucasdecamposranzani@gmail.com>
2026-02-03 18:35:57 -0300

    feat(ms-login): Implements model User and Role

---
+++ login/src/main/java/com/api/canvas/student/login/entities/User.java
@@ -0,0 +1,51 @@
+package com.api.canvas.student.login.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "user_canvas_id", length = 500, nullable = false, unique = true)
+    private String userCanvasId;
+
+    @Column(name = "token_canvas",nullable = false)
+    private String tokenCanvas;
+
+    @Column(nullable = false)
+    private String university;
+
+    @Column(nullable = false)
+    private String course;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+}
