9854323de8545c9b46fd062a3550fb0bcd786e1a
llucascr <lucasdecamposranzani@gmail.com>
2026-02-04 18:29:50 -0300

    refactor(CanvasStudent): delete CanvaService

--- canvas.student/src/main/java/com/api/canvas/student/dto/response/user/UserIdResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.api.canvas.student.dto.response.user;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record UserIdResponseDTO(
-        String id,
-        String name
-) {}
